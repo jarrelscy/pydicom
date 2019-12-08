b0e3a5b7a4ba9988c8638e33ddaa8a5da9818856
Jarrel Seah <jarrelscy@gmail.com>
2019-12-08 23:31:30 +1100

    Update pillow_handler.py

--- pydicom/pixel_data_handlers/pillow_handler.py
+++ pydicom/pixel_data_handlers/pillow_handler.py
@@ -9,6 +9,7 @@ import warnings
 
 try:
     import numpy
+    import numpy as np
     HAVE_NP = True
 except ImportError:
     HAVE_NP = False
@@ -88,6 +89,12 @@ def should_change_PhotometricInterpretation_to_RGB(ds):
     should_change = ds.SamplesPerPixel == 3
     return False
 
+def read_uint12(data_chunk):
+    data = np.frombuffer(data_chunk, dtype=np.uint8)
+    fst_uint8, mid_uint8, lst_uint8 = np.reshape(data, (data.shape[0] // 3, 3)).astype(np.uint16).T
+    fst_uint12 = (fst_uint8 << 4) + (mid_uint8 >> 4)
+    snd_uint12 = ((mid_uint8 % 16) << 8) + lst_uint8
+    return np.reshape(np.concatenate((fst_uint12[:, None], snd_uint12[:, None]), axis=1), 2 * fst_uint12.shape[0])
 
 def get_pixeldata(ds):
     """Return a :class:`numpy.ndarray` of the *Pixel Data*.
